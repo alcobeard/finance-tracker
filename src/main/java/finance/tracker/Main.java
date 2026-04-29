b93457aa4512b40214ae76c135df80dcfa24dde2
alcobeard <alcobeard@gmail.com>
2026-04-29 13:59:03 +0300

    step 0

---
+++ src/main/java/finance/tracker/Main.java
@@ -0,0 +1,23 @@
+package finance.tracker;
+
+/**
+ * @author Aleksandr Bagdasarov
+ * 2026-04
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        /***
+         * Изменить этот метод, чтобы он выводил сообщение вида:
+         * Welcome to Finance Tracker
+         * 1. Add transaction
+         * 2. List transactions
+         * 3. Exit
+         *
+         * Никакого функционала на текущий момент за этим не должно быть. Просто запуск приложения -> вывод нового сообщения -> завершение работы
+         *
+         * Обратите внимание, что должно быть 4 строки
+         */
+        System.out.println("Hello World!");
+    }
+}
