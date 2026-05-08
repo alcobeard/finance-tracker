7f2aa49cf96d9cda76155a169ae0a7dd807bd88f
Zergx <hitriylis666@gmail.com>
2026-05-08 15:32:01 +0300

    Задание №1

--- src/main/java/finance/tracker/FinanceService.java
+++ src/main/java/finance/tracker/FinanceService.java
@@ -3,6 +3,7 @@ package finance.tracker;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author Aleksandr Bagdasarov
@@ -11,39 +12,165 @@ import java.util.List;
 
 public class FinanceService {
     public static void main(String[] args) {
-        int balance = 0;
+        showMenu();
+        ArrayList<HashMap<String, String>> transactions = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
         boolean running = true;
-
-        // Разобраться, что это за структура. Как она выглядит, для чего она и как с ней работать
-        List<HashMap<String, String>> transactions = new ArrayList<>();
-//      Давайте пока что остановимся на текущем формате хранения/входных данных
-//        {
-//            "id"       -> "1",
-//            "type"     -> "EXPENSE",
-//            "amount"   -> "500",
-//            "date"     -> "2026-04-29",
-//        }
-
-        /**
-         * Реализовать функионал финансового трекера с взаимодействием через консоль
-         *
-         * Функционал
-         * 1. Добавить доход/расход
-         * 2. Вывести список операций
-         * 3. Показать баланс
-         * 4. Удалить операцию по id
-         * 5. Выйти из программы
-         */
-        while (running) { // Наша программа должна работать, пока мы не захотим из нее выйти. Подумайте,
-            // как нам показывать меню до тех пор, пока не будет выбрано выйти из программы
-            showMenu();
-
+        while (running){
+            try {
+                int choice = Integer.parseInt(scanner.nextLine());              // сначала был while (choice != 5), но потом из за проблем с валидацей пришлось отказаться
+                switch (choice) {                                                   // добавил case 5:
+                    case 1:
+                        while (true) {
+                            try {
+                                HashMap<String, String> transactionAdd = new HashMap<>();
+                                System.out.println("------------------------------------------");
+                                System.out.println("==Трекер бюджета | Добавление транзакции==");
+                                System.out.println("------------------------------------------");
+                                System.out.print("Введите сумму вашего дохода или расхода: ");
+                                int amount = Integer.parseInt(scanner.nextLine());
+                                if (amount > 0) {
+                                    String type = new String(); //scanner.nextLine();
+                                    while (true) {
+                                        System.out.println("------------------------------------------");
+                                        System.out.print("Введите тип транзакции (доход/расход): ");
+                                        String input = scanner.nextLine();
+                                        if (input.equalsIgnoreCase("доход") || input.equalsIgnoreCase("расход")) {
+                                            type = input.toLowerCase();
+                                            break;
+                                        } else {
+                                            System.out.println("------------------------------------------");
+                                            System.out.println("Ошибка ввода!");
+                                        }
+                                    }
+                                    String date = new String();
+                                    while (true) {
+                                        System.out.println("------------------------------------------");
+                                        System.out.print("Введите дату транзакции (формат: дд-мм-гггг / дд.мм.гггг): ");
+                                        String input = scanner.nextLine();
+                                        if (input.matches("\\d{2}-\\d{2}-\\d{4}") || (input.matches("\\d{2}.\\d{2}.\\d{4}"))) {
+                                            date = input;
+                                            break;
+                                        } else {
+                                            System.out.println("------------------------------------------");
+                                            System.out.println("Ошибка ввода!");
+                                        }
+                                    }
+                                    int newId = Integer.parseInt(String.valueOf(transactions.size() + 1)); // тут я хотел изначально сделать пиздец какие проверки написав циклы, но придумал такое элегантное решение (идея моя, помог deepseek)
+                                    transactionAdd.put("id", String.valueOf(newId));
+                                    transactionAdd.put("type", type);
+                                    transactionAdd.put("amount", String.valueOf(amount));
+                                    transactionAdd.put("date", date);
+                                    transactions.add(transactionAdd);
+                                    System.out.println("------------------------------------------");
+                                    System.out.println("       Транзакция успешно добавлена!      ");
+                                    System.out.println("------------------------------------------");
+                                    // System.out.println("Транзакция добавлена: " +transaction1.get("id") +" " +transaction1.get("type"));
+                                    System.out.printf("| %-4s | %-9s | %-6s | %-10s | %n", "ID", "Сумма", "Тип", "Дата");
+                                    System.out.println("|------|-----------|--------|------------|");
+                                    System.out.printf("| %-4s | %-9s | %-6s | %-10s | %n", transactionAdd.get("id"), transactionAdd.get("amount"), transactionAdd.get("type"), transactionAdd.get("date"));
+                                    System.out.println("------------------------------------------");
+                                    showMenu();
+                                    break;
+                                }
+                            } catch (NumberFormatException ex) {
+                                System.out.println("------------------------------------------");
+                                System.out.println("Ошибка ввода!");
+                            }
+                        }
+                        break;
+                    case 2:
+                        if (transactions.isEmpty()) {
+                            System.out.println("------------------------------------------");
+                            System.out.println("Список операций пуст!");
+                            System.out.println("------------------------------------------");
+                        } else {
+                            System.out.println("------------------------------------------");
+                            System.out.printf("| %-4s | %-9s | %-6s | %-10s | %n", "ID", "Сумма", "Тип", "Дата");
+                            System.out.println("|------|-----------|--------|------------|");
+                            for (int i = 0; i < transactions.size(); i++) {
+                                System.out.printf("| %-4s | %-9s | %-6s | %-10s | %n", transactions.get(i).get("id"), transactions.get(i).get("amount"), transactions.get(i).get("type"), transactions.get(i).get("date"));
+                                System.out.println("|------|-----------|--------|------------|");
+                            }
+                        }
+                        showMenu();
+                        break;
+                    case 3:
+                        System.out.println("------------------------------------------");
+                        System.out.println("===  Трекер бюджета | Рассчёт прибыли  ===");
+                        int prices = 0;
+                        if (transactions.isEmpty()) {                          // transactions.size = 0 не работает
+                            System.out.println("------------------------------------------");
+                            System.out.println("Список операций пуст!");
+                            System.out.println("------------------------------------------");
+                        } else {
+                            for (int i = 0; i < transactions.size(); i++) {
+                                if (transactions.get(i).get("type").equals("доход")) {                        // (transactions.get(i).get("type") == "доход" не работает
+                                    prices = prices + Integer.parseInt(transactions.get(i).get("amount"));
+                                } else {
+                                    prices = prices - Integer.parseInt(transactions.get(i).get("amount"));
+                                }
+                            }
+                            System.out.println("Ваша прибыль составляет: " + prices);
+                        }
+                        showMenu();
+                        break;
+                    case 4:
+                        System.out.println("------------------------------------------");
+                        System.out.print("Введите ID операции: ");
+                        String input = scanner.nextLine();
+                        boolean found = false;
+                        for (int i = 0; i < transactions.size(); i++) {                   //долго ломал голову, не знал, что if без else работает
+                            if (transactions.get(i).get("id").equals(input)) {
+                                transactions.remove(i);
+                                found = true;
+                                System.out.println("------------------------------------------");
+                                System.out.println("Операция с ID: " + input +" удалена!");
+                                System.out.println("------------------------------------------");
+                                break;
+                            }
+                        }
+                        if (found == false) {
+                            System.out.println("------------------------------------------");
+                            System.out.println("Операции с ID:" + input + " не существует!");
+                            System.out.println("------------------------------------------");
+                        }
+                        showMenu();
+                        break;
+                    case 5:
+                        System.out.println("------------------------------------------");
+                        System.out.println("              До свидания!                ");
+                        System.out.println("------------------------------------------");
+                        return;
+                    default:
+                        System.out.println("------------------------------------------");
+                        System.out.println("Выберете пункт меню!");
+                        System.out.println("------------------------------------------");
+                        showMenu();
+                        break;
+                }
+            }
+            catch(NumberFormatException e) {
+                System.out.println("------------------------------------------");
+                System.out.println("Ошибка ввода!");
+                System.out.println("------------------------------------------");
+                System.out.println("Выберете пункт меню!");
+                System.out.println("------------------------------------------");
+                showMenu();
+            }
+//        choice = Integer.parseInt(scanner.nextLine());
         }
-
     }
 
-    // Давайте выведим демонстрацию меню в отдельную функцию для удобства.
     public static void showMenu() {
-
+        System.out.println("========= Трекер Вашего бюджета ==========");
+        System.out.println("------------------------------------------");
+        System.out.println("1. Добавить доход или расход");
+        System.out.println("2. Отобразить список всех операций");
+        System.out.println("3. Расчитать прибыль");
+        System.out.println("4. Удалить операцию по ID");
+        System.out.println("5. Выход");
+        System.out.println("------------------------------------------");
+        System.out.print("Пункт меню № ");
     }
 }
