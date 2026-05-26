1a3eec588a34f39f437d90d8e75fc0b57629a87f
LifeguardLemonade <hardiydenver@gmail.com>
2026-05-26 19:28:53 +0300

    Объекты, методы, enum

--- src/main/java/finance/tracker/dto/Transaction.java
+++ src/main/java/finance/tracker/dto/Transaction.java
@@ -1,16 +1,31 @@
 package finance.tracker.dto;
-
 import java.time.LocalDateTime;
-
-/**
- * @author Aleksandr Bagdasarov
- * 2026-05
- */
-
 public class Transaction {
-    int id;
-    Double amount;
-    LocalDateTime date;
-    TransactionType type;
-    Category category;
+    private int id;
+    private double amount;
+    private LocalDateTime date;
+    private TransactionType type;
+    private Category category;
+    public Transaction(int id, double amount, LocalDateTime date, TransactionType type, Category category) {
+        this.id = id;
+        this.amount = amount;
+        this.date = date;
+        this.type = type;
+        this.category = category;
+    }
+    public int getId() {
+        return id;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public LocalDateTime getDate() {
+        return date;
+    }
+    public TransactionType getType() {
+        return type;
+    }
+    public Category getCategory() {
+        return category;
+    }
 }
